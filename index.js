0c06575a13d2a80467170bedf59be537e37246dd
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-04-24 15:26:19 -0300

    Corrige uso do sendFile do express

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = new express();
+const path = require('path');
 
 app.get('/', function(request, response){
-    response.sendFile('index.html');
+    response.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3005)
+app.listen(3009)
